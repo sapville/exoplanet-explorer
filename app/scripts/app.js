aac142a0c9790e0495012bf4cb26261dc08db174
Evgenii Fedoseev <abap@sapville.com>
2017-11-20 21:35:25 +0300

    finish lesson 1

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,7 +10,7 @@ Instructions:
 // Inline configuration for jshint below. Prevents `gulp jshint` from failing with quiz starter code.
 /* jshint unused: false */
 
-(function(document) {
+(function (document) {
   'use strict';
 
   var home = null;
@@ -19,7 +19,7 @@ Instructions:
    * Helper function to show the search query.
    * @param {String} query - The search query.
    */
-  function addSearchHeader(query) {
+  function addSearchHeader (query) {
     home.innerHTML = '<h2 class="page-title">query: ' + query + '</h2>';
   }
 
@@ -28,13 +28,14 @@ Instructions:
    * @param  {String} url - The URL to fetch.
    * @return {Promise}    - A Promise that resolves when the XHR succeeds and fails otherwise.
    */
-  function get(url) {
+  function get (url) {
     /*
     Use the Fetch API to GET a URL.
     Return the fetch.
 
     Your code goes here!
      */
+    return fetch(url, {method: 'get'});
   }
 
   /**
@@ -42,15 +43,18 @@ Instructions:
    * @param  {String} url - The JSON URL to fetch.
    * @return {Promise}    - A promise that passes the parsed JSON response.
    */
-  function getJSON(url) {
+  function getJSON (url) {
     /*
     Return a Promise that gets a URL and parses the JSON response. Use your get method!
 
     Your code goes here!
      */
+    return get(url
+    ).then(function (response) {return response.json();}
+    );
   }
 
-  window.addEventListener('WebComponentsReady', function() {
+  window.addEventListener('WebComponentsReady', function () {
     home = document.querySelector('section[data-route="home"]');
     /*
     Uncomment the next line when you're ready to test!
@@ -58,6 +62,13 @@ Instructions:
 
     Your code goes here too!
      */
-    // getJSON('../data/earth-like-results.json')
+    getJSON('../data/earth-like-results.json'
+    ).then(function (response) {
+      console.log(response);
+      addSearchHeader(response.query);
+    }).catch(function (error) {
+      console.log(error);
+      addSearchHeader('unknown');
+    });
   });
 })(document);
