0eca77c9b70df401692d6ceb921f98ce6227255f
Evgenii Fedoseev <abap@sapville.com>
2017-11-20 14:57:17 +0300

    put GET inside Promise

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true,
+        "jquery": true,
+        "jasmine": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-console": "off",
+        "no-unused-vars": "off",
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,7 +9,7 @@ Instructions:
 // Inline configuration for jshint below. Prevents `gulp jshint` from failing with quiz starter code.
 /* jshint unused: false */
 
-(function(document) {
+(function (document) {
   'use strict';
 
   var home = null;
@@ -18,7 +18,7 @@ Instructions:
    * Helper function to show the search query.
    * @param {String} response - The unparsed JSON response from get.
    */
-  function addSearchHeader(response) {
+  function addSearchHeader (response) {
     try {
       response = JSON.parse(response).query;  // you'll be moving this line out of here in the next quiz!
     } catch (e) {
@@ -32,35 +32,48 @@ Instructions:
    * @param  {String} url - The URL to fetch.
    * @return {Promise}    - A Promise that resolves when the XHR succeeds and fails otherwise.
    */
-  function get(url) {
+  function get (url) {
     /*
     This code needs to get wrapped in a Promise!
      */
-    var req = new XMLHttpRequest();
-    req.open('GET', url);
-    req.onload = function() {
-      if (req.status === 200) {
-        // It worked!
-        // You'll want to resolve with the data from req.response
-      } else {
+
+    return new Promise(function (resolve, reject) {
+
+      const req = new XMLHttpRequest();
+      req.open('GET', url);
+      req.onload = function () {
+        if (req.status === 200) {
+          // It worked!
+          // You'll want to resolve with the data from req.response
+          resolve(req.response);
+        } else {
+          // It failed :(
+          // Be nice and reject with req.statusText
+          reject(Error(req.statusText));
+        }
+      };
+      req.onerror = function () {
         // It failed :(
-        // Be nice and reject with req.statusText
-      }
-    };
-    req.onerror = function() {
-      // It failed :(
-      // Pass a 'Network Error' to reject
-    };
-    req.send();
+        // Pass a 'Network Error' to reject
+        reject(Error('Network Error'));
+      };
+      req.send();
+    });
   }
 
-  window.addEventListener('WebComponentsReady', function() {
+  window.addEventListener('WebComponentsReady', function () {
     home = document.querySelector('section[data-route="home"]');
     /*
     Uncomment the next line you're ready to start chaining and testing!
     You'll need to add a .then and a .catch. Pass the response to addSearchHeader on resolve or
     pass 'unknown' to addSearchHeader if it rejects.
      */
-    // get('../data/earth-like-results.json')
+    get('../data/earth-like-results.json'
+    ).then(function (response) {
+      addSearchHeader(response);
+    }).catch(function (error) {
+      addSearchHeader('unknown');
+      console.log(error);
+    });
   });
 })(document);
